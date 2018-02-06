9b3ac5e346383f34d8f319bede2e9174dda9ca6e
r3b311i0n <nasashinega@gmail.com>
2018-02-06 23:44:43 +0530

    Conflate current song info command into single command

--- src/bin/player-cli.rs
+++ src/bin/player-cli.rs
@@ -4,12 +4,21 @@ extern crate mpd;
 use mpd::Client;
 use std::net::TcpStream;
 use std::env;
-use mpd_rs_interface::{get_current_info, next, pause, play, prev, stop, update};
+use mpd_rs_interface::{get_tag, next, pause, play, prev, stop, update};
 
 
 fn main() {
     let conn: Client<TcpStream> = Client::connect("127.0.0.1:6600").unwrap();
     parse_cmd_args(conn);
+}
+
+fn get_current_info(mut conn: Client) {
+    let title = get_tag(&mut conn, "title");
+    let album = get_tag(&mut conn, "album");
+    let artist = get_tag(&mut conn, "artist");
+    let duration = get_tag(&mut conn, "duration");
+    let file = get_tag(&mut conn, "file");
+    println!("{}\n{}\n{}\n{}\n{}\n", title, album, artist, duration, file);
 }
 
 fn parse_cmd_args(conn: Client) {
@@ -25,12 +34,12 @@ fn parse_cmd_args(conn: Client) {
                 "next" | "ns" => next(conn),
                 "prev" | "ps" => prev(conn),
                 "update" => update(conn),
-                "file" => get_current_info(conn, "file"),
-                "stream-name" => get_current_info(conn, "stream-name"),
-                "title" | "ct" => get_current_info(conn, "title"),
-                "album" | "cal" => get_current_info(conn, "album"),
-                "artist" | "cart" => get_current_info(conn, "artist"),
-                "duration" => get_current_info(conn, "duration"),
+                "info" | "i" => get_current_info(conn),
+//                "file" => get_current_info(conn, "file"),
+//                "title" | "ct" => get_current_info(conn),
+//                "album" | "cal" => get_current_info(conn, "album"),
+//                "artist" | "cart" => get_current_info(conn, "artist"),
+//                "duration" => get_current_info(conn, "duration"),
                 _ => ()
             }
         }
