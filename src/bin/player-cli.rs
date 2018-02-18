5e38d341366978d953fc6d9126e0799fd1447e81
r3b311i0n <nasashinega@gmail.com>
2018-02-18 20:41:16 +0530

    Add help message

--- src/bin/player-cli.rs
+++ src/bin/player-cli.rs
@@ -22,13 +22,28 @@ fn get_current_info(mut conn: Client) {
     println!("{}\n{}\n{}\n{}\n{}\n", title, album, artist, duration, file);
 }
 
+fn show_help() {
+    println!(
+        "\nh ⇾ Show This\n\
+        play | s ⇾ Play\n\
+        pause | p ⇾ Pause\n\
+        stop ⇾ Stop\n\
+        next | ns ⇾ Next Song\n\
+        prev | ps ⇾ Previous Song\n\
+        update ⇾ Update Database\n\
+        info | i ⇾ Get Current Song's Information\n"
+    );
+}
+
 fn parse_cmd_args(conn: Client) {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
+        1 => show_help(),
         2 => {
             let cmd = &args[1].to_lowercase();
             match &cmd[..] {
+                "h" => show_help(),
                 "play" | "s" => play(conn),
                 "pause" | "p" => pause(conn),
                 "stop" => stop(conn),
